efd980f671e861ab76b4c925b9b8c15a810d7e2f
LuisMeirelles <luis.gustavo.souto.meirelles@gmail.com>
2020-08-08 15:30:43 -0300

    implementation of get and post methods in the /classes route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 
+import routes from './routes'
+
 const app = express()
 
-app.get('/', (req, res) => {
-	return res.json({ message: 'hello world' })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
