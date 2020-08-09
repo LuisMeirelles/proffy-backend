8ead8497348ad16f5a7ddbf39d14d0f4a9cf6ba5
LuisMeirelles <luis.gustavo.souto.meirelles@gmail.com>
2020-08-09 19:26:36 -0300

    environment variables implementation

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,17 @@
 import knex from 'knex'
+import 'dotenv/config'
+
+const host = process.env.DB_HOST
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_NAME
 
 export default knex({
 	client: 'mysql',
 	connection: {
-		host: '127.0.0.1',
-		user: 'root',
-		password: 'mywordismypassword',
-		database: 'proffy'
+		host,
+		user,
+		password,
+		database
 	}
 })
