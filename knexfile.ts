34e244493da539e1c7e702574b47058f585222c9
Luis Meirelles <36140883+LuisMeirelles@users.noreply.github.com>
2021-07-07 03:01:37 -0300

    Update knexfile.ts

--- knexfile.ts
+++ knexfile.ts
@@ -3,10 +3,10 @@ import path from 'path'
 module.exports = {
 	client: 'mysql',
 	connection: {
-		host : '127.0.0.1',
-		user : 'root',
-		password: 'mywordismypassword',
-		database: 'proffy'
+		host : process.env.DB_HOST || '127.0.0.1',
+		user : process.env.DB_USER || 'root',
+		password: process.env.DB_PASSWORD || 'mywordismypassword',
+		database: process.env.DB_NAME || 'proffy'
 	},
 	migrations: {
 		directory: path.resolve(__dirname, 'src', 'database', 'migrations')
